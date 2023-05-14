f0c849e8da222bcaffba5ba25d00b97679b4dd3c
omisha <os4624@srmist.edu.in>
2023-05-14 14:30:25 +0530

    Time: 15 ms (47.59%) | Memory: 21.7 MB (80.33%) - LeetSync

---
+++ validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isBST(TreeNode* root, long minVal, long maxVal)
+    {
+        if(root==NULL)
+            return true;
+        if(root->val >= maxVal || root->val <= minVal)
+            return false;
+        return isBST(root->left,minVal,root->val) && isBST(root->right,root->val,maxVal);
+    }
+    
+    bool isValidBST(TreeNode* root) {
+        return isBST(root,LONG_MIN,LONG_MAX);
+    }
+};

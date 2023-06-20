30805007dc71af03f45e98f3eb2cc6f7a39ece1e
omisha <os4624@srmist.edu.in>
2023-06-20 22:02:13 +0530

    Time: 11 ms (73.68%) | Memory: 8.2 MB (19.95%) - LeetSync

---
+++ linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  ListNode *floydcycle(ListNode *head)
+  {
+      if(head==NULL)
+      {return NULL;}
+
+      ListNode *slow=head;
+      ListNode *fast=head;
+      
+      while(slow!=NULL && fast!=NULL)
+      {
+          fast=fast->next;
+          if(fast!=NULL)
+          fast=fast->next;
+          slow=slow->next;
+          if(slow==fast)
+          return slow;
+      }
+      return NULL;
+
+  }
+    bool hasCycle(ListNode *head) {
+     return floydcycle(head);
+    }
+};

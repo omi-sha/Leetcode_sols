8f6a1bd8434bae1ca9b6e5af4647653a8d728df4
omisha <os4624@srmist.edu.in>
2023-07-22 15:03:24 +0530

    Time: 3 ms (97.40%) | Memory: 11.1 MB (19.20%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> s;
+        for(auto a:arr)
+        {
+            s.insert(a);
+        }
+        for(auto s1:s)
+        {
+            if(s1 !=0 && s.find(s1*2)!=s.end()) return true;
+        }
+        return count(arr.begin(),arr.end(),0)>1;
+
+    }
+};
